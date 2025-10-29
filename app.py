742c89b0dc0a393387a55755e349d762be11860b
Nathan4043 <nathanr88@nycstudents.net>
2025-10-29 10:01:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+# for movies in data:
+#     print(movies["title"], movies["year"])
+
+
+# Age=(int(input("input year, and movie after is printed")))
+# for movies in data:
+#     if Age < movies["year"]:
+#         print(movies["title"],movies["year"])
+
+
+m = (int(input("input year, and movie after is printed")))
+          
+
+
